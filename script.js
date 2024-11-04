b969e257fa0fd121ef6f3d262b754b4f22b20d41
patrickasdf <173692702+patrickasdf@users.noreply.github.com>
2024-11-05 02:02:42 +1000

    grid resize, hover, click to toggle dark on/off added

--- script.js
+++ script.js
@@ -1,20 +1,70 @@
 const container = document.querySelector(".container");
 
-let divSize = 16; // div grid size (16x16 etc.)
+let divSize = 16; // initial grid size (16x16)
 
-function createDivWidth(divSize) {
+const buttonGrid = document.createElement("button");
+buttonGrid.textContent = "Enter new grid size - Current grid " + divSize + 
+    " x " + divSize;;
+document.body.appendChild(buttonGrid);
+document.body.appendChild(container);
+
+function createGrid(divSize) {
     for (i = 0; i < divSize; i++) {
         const divWidth = document.createElement("div");
         divWidth.classList.add("divWidth");
-        divWidth.style.width = 800 / divSize + "px";
-        divWidth.textContent = i + "-div";
         container.appendChild(divWidth);
         for (j = 0; j < divSize; j++) {
             const divHeight = document.createElement("div");
             divHeight.classList.add("divHeight");
-            divHeight.textContent = "div-" + j;
             divWidth.appendChild(divHeight);
         }
     }
 };
-createDivWidth(divSize);
+createGrid(divSize);
+
+function gridHover() {
+    const gridCubes = document.querySelectorAll("div.divHeight");
+    gridCubes.forEach((div) => {
+        div.addEventListener('mouseover', () => {
+            div.classList.add("hovered");
+        });
+        div.addEventListener('mouseout', () => {
+            div.classList.remove('hovered');
+        })
+    });
+}
+gridHover();
+
+function gridClick() {
+    const gridCubes = document.querySelectorAll("div.divHeight");
+    gridCubes.forEach((div) => {
+        div.addEventListener('click', () => {
+            if (!div.classList.contains("clicked")) {
+                div.classList.add("clicked");    
+            }
+            else {
+                div.classList.remove("clicked");
+            }
+        });
+    });
+}
+gridClick();
+
+// new clickEvent = CustomEvent() 
+
+buttonGrid.addEventListener('click', () => {
+    console.log("btn click");
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+    divSize = parseInt(prompt("Enter new grid size (Maximum 100): "));
+    while (divSize > 100) {
+        alert("Maximum grid size is 100");
+        divSize = parseInt(prompt("Enter new grid size: "));
+    }
+    buttonGrid.textContent = "Enter new grid size - Current grid " + divSize + 
+        " x " + divSize;
+    createGrid(divSize);
+    gridHover();
+    gridClick();
+});
